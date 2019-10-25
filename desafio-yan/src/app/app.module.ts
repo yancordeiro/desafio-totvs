325e9dfb69f860057c6abb3092991a2b6305b309
Yan Cordeiro <yandemoraiscordeiro@gmail.com>
2019-10-25 04:16:00 -0300

    Final project

--- desafio-yan/src/app/app.module.ts
+++ desafio-yan/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule} from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,7 +31,8 @@ import { FormsModule } from '@angular/forms';
     ]),
     BrowserAnimationsModule,
     MatDialogModule,
-    FormsModule
+    FormsModule,
+    MatExpansionModule
   ],
   entryComponents: [
     MyDialogComponent
